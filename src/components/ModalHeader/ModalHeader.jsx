f6afeae78da0e37fef3f1eb862705e531be10776
IOverclocked <osiatycki.m@gmail.com>
2019-04-04 23:36:36 +0200

    correct styles for input

---
+++ src/components/ModalHeader/ModalHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles form './ModalHeader.modules.scss';
+
+const ModalHeader = ({ title }) => (
+    <header>
+
+    </header>
+)
+
+export default ModalHeader;
