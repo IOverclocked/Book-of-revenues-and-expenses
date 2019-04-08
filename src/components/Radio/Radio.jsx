2728149e3db3edd148846d4df21e352acbc32634
IOverclocked <osiatycki.m@gmail.com>
2019-04-08 13:06:27 +0200

    Styled radio input

--- src/components/Radio/Radio.jsx
+++ src/components/Radio/Radio.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import styles from './Radio.module.scss';
 
-const Radio = ({ input, name, label, value, checked }) => (
+const _setCircleInClassName = (name) => {
+    if (name === 'expenses') {
+        return styles.circle__expenses;
+    } else {
+        return styles.circle__revenues;
+    }
+}
 
+const Radio = ({ input, name, label, value, checked }) => (
     <label className={styles.wrapper}>
         <input 
             {...input}
             name={name}
             type="Radio"
-            className={styles.input}
             label={label}
             value={value}
             checked={checked || value === input.value ? true: false}
+            className={styles.input}
         />
-        &nbsp;{label}
+        <div className={styles.circle__out}>
+            <div className={_setCircleInClassName(value)}>
+
+            </div>
+        </div>
+        <span className={styles.label}>&nbsp;{label}</span>
     </label>
 )
 
