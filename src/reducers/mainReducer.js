257d5c149f981787e575eac166bfadc130b9dcd8
IOverclocked <osiatycki.m@gmail.com>
2019-04-09 09:43:14 +0200

    Create simple localstorage

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,11 +1,17 @@
 
+const l = localStorage.getItem('Main');
+console.log(l);
+
+const initState = l ? JSON.parse(l) : [];
+
 const add = (state, action) => {
     return [...state, action.newItem];
 }
 
-const main = (state = [], action) => {
+const main = (state = initState, action) => {
     switch (action.type) {
         case 'ADD':
+            localStorage.setItem('Main', JSON.stringify(add(state, action)));
             return add(state, action);
         default:
             return state;
--- src/reducers/resultReducer.js
+++ src/reducers/resultReducer.js
@@ -1,14 +1,14 @@
-
-const initState = {
+const r = localStorage.getItem('Result');
+const initState = r ? JSON.parse(r) : {
     result: 0,
     expenses: 0,
     revenues: 0
-};
+}
 
 const expenses = (state, action) => {
     return {
         ...state,
-        result: state.result - action.expenses,
+        result: (state.result - action.expenses).toFixed(2),
         expenses: state.expenses + action.expenses
     }
 }
@@ -24,8 +24,10 @@ const revenues = (state, action) => {
 const result = (state = initState, action) => {
     switch (action.type) {
         case 'EXPENSES':
+            localStorage.setItem('Result', JSON.stringify(expenses(state, action)));
             return expenses(state, action);
         case 'REVENUES':
+            localStorage.setItem('Result', JSON.stringify(revenues(state, action)));
             return revenues(state, action);
         default:
             return state;
