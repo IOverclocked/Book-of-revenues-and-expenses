a95866dac48e9694b5bc927697d574cf1155cdd4
IOverclocked <osiatycki.m@gmail.com>
2019-04-24 18:36:58 +0200

    added animate icons in side menu

--- src/views/MenuView/MenuView.jsx
+++ src/views/MenuView/MenuView.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styles from './MenuView.module.scss';
+import Money from '@material-ui/icons/AttachMoney';
 // import PropTypes from 'prop-types';
 
 const {
     wrapper__open,
     wrapper__close,
     menu__open,
-    menu__close
+    menu__close,
+    wrapper__icons,
+    icon__close,
+    icon__open,
 } = styles;
 
-const MenuView = ({ toggleSideMenuControl }) => (
-    <div className={toggleSideMenuControl ? wrapper__open : wrapper__close}>
-        <nav className={toggleSideMenuControl ? menu__open : menu__close}>
-            1
+const MenuView = ({ toggleSideMenuControl: isOpen }) => (
+    <div className={isOpen ? wrapper__open : wrapper__close}>
+        <nav className={isOpen ? menu__open : menu__close}>
+            <header className={wrapper__icons}>
+                <Money className={isOpen ? icon__open : icon__close} />
+                <Money className={isOpen ? icon__open : icon__close} />
+            </header>
         </nav>
     </div>
 )
