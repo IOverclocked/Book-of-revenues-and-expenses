b36046c57e66ba6dc478531922117e694c8098c5
IOverclocked <osiatycki.m@gmail.com>
2019-04-24 18:12:26 +0200

    improved styles side menu

--- src/views/MenuView/MenuView.jsx
+++ src/views/MenuView/MenuView.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './MenuView.module.scss';
 // import PropTypes from 'prop-types';
 
-const { wrapper__open, wrapper__close, menu__open, menu__close } = styles;
+const {
+    wrapper__open,
+    wrapper__close,
+    menu__open,
+    menu__close
+} = styles;
 
 const MenuView = ({ toggleSideMenuControl }) => (
     <div className={toggleSideMenuControl ? wrapper__open : wrapper__close}>
