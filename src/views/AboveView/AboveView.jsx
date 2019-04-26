94682f37a3dfeccc192a24448e0abcd44f0af599
IOverclocked <osiatycki.m@gmail.com>
2019-04-26 16:22:46 +0200

    Add new view Above

---
+++ src/views/AboveView/AboveView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AboveView.modules.scss';
+
+function AboveView() {
+    return (
+        <div className={styles.wrapper}>
+            Above
+        </div>
+    )
+}
+
+export default AboveView;
