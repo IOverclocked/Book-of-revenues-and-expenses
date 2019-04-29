b22f48473b95f272fd249364a8ee2a3b6029cb96
IOverclocked <osiatycki.m@gmail.com>
2019-04-29 18:37:52 +0200

    Create stateDetails components

---
+++ src/components/StateDetails/StateDetails.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './StateDetails.module.scss';
+import PropTypes from 'prop-types'
+
+function StateDetails({ ...props }) {
+    return (
+        <div className={styles.wrapper}>
+            <section className={styles.state}>
+                <h2 className={styles.cashState}>
+                    4358
+                </h2>
+            </section>
+            <section className={styles.stateDetails}>
+                <div className={styles.revenues}>
+                    <h3 className={styles.title}>Revenues</h3>
+                    <span>1299</span>
+                </div>
+                <div className={styles.date}>
+                    <h3>to 21-01-2019</h3>
+                </div>
+                <div className={styles.expenses}>
+                    <h3 className={styles.title}>Expenses</h3>
+                    <span>1299</span>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+StateDetails.propTypes = {
+
+}
+
+export default StateDetails;
+
--- src/views/DetailsView/DetailsView.jsx
+++ src/views/DetailsView/DetailsView.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './DetailsView.module.scss';
 import Header from '../../components/Header/Header';
+import StateDetails from '../../components/StateDetails/StateDetails';
 
 function DetailsView(props) {
     return (
         <div className={styles.wrapper}>
             <Header />
-            Details
+            <StateDetails />
         </div>
     )
 }
