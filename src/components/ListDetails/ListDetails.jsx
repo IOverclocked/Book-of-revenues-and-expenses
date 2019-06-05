671cb7312f2535963a495288df17d87e766c0993
IOverclocked <osiatycki.m@gmail.com>
2019-06-05 13:10:43 +0200

    New component item details

--- src/components/ListDetails/ListDetails.jsx
+++ src/components/ListDetails/ListDetails.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ListItemDetails from 'components/ListItemDetails/ListItemDetails';
 import styles from './ListDetails.module.scss';
 
 const ListDetails = ({ isExpenses, list }) => (
-  <section className={isExpenses ? styles.expenses : styles.revenues}>
+  <section className={isExpenses ? styles.wrapper__expenses : styles.wrapper__revenues}>
     <header className={styles.header}>
       <h2 className={styles.title}>{isExpenses ? 'Expenses' : 'Revenues'}</h2>
     </header>
     <ul className={styles.list}>
-      {list.map(el => (
-        <li key={el.id} />
+      {list.map(({ id, title, date, cash }) => (
+        <ListItemDetails key={id} title={title} date={date} cash={cash} isExpenses={isExpenses} />
       ))}
     </ul>
   </section>
---
+++ src/components/ListItemDetails/ListItemDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ListItemDetails.module.scss';
+
+const ListItemDetails = ({ title, date, cash, isExpenses }) => (
+  <li className={isExpenses ? styles.wrapper__expenses : styles.wrapper__revenues}>
+    <div className={styles.header}>
+      <span>{title}</span>
+      <span>{date}</span>
+    </div>
+    <div className={styles.cash}>{cash}</div>
+  </li>
+);
+
+ListItemDetails.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  cash: PropTypes.string.isRequired,
+  isExpenses: PropTypes.bool,
+};
+
+ListItemDetails.defaultProps = {
+  isExpenses: false,
+};
+
+export default ListItemDetails;
