70f5e6aeeda6e6d755d7f7fc2d5e2e7f782a23a3
IOverclocked <osiatycki.m@gmail.com>
2019-06-05 12:10:52 +0200

    Create details list
    Adding from redux props
    to details list view and details
    view changed to class
    components.

--- src/components/ListDetails/ListDetails.jsx
+++ src/components/ListDetails/ListDetails.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ListDetails.module.scss';
 
-const ListDetails = ({ isExpenses }) => (
+const ListDetails = ({ isExpenses, list }) => (
   <section className={isExpenses ? styles.expenses : styles.revenues}>
     <header className={styles.header}>
       <h2 className={styles.title}>{isExpenses ? 'Expenses' : 'Revenues'}</h2>
     </header>
-    {/* <ul className={styles.list}> */}
-    {/* {list.map(el => <li key={el.id}></li>)} */}
-    {/* </ul> */}
+    <ul className={styles.list}>
+      {list.map(el => (
+        <li key={el.id} />
+      ))}
+    </ul>
   </section>
 );
 
 ListDetails.propTypes = {
   isExpenses: PropTypes.bool,
-  // list: PropTypes.arrayOf(PropTypes.object.isRequired),
+  list: PropTypes.arrayOf(PropTypes.object.isRequired),
 };
 
 ListDetails.defaultProps = {
-  // list: [],
+  list: [],
   isExpenses: false,
 };
 
